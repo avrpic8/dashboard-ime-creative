0bb622475e8505846036e3dc364cfa49ba277a40
saeed <avrpic8@gmail.com>
2021-06-10 11:21:50 +0430

    first commit

---
+++ template/dashboard/index.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="../../assets/css/all.css">
+    <link rel="stylesheet" href="../../assets/css/styles.css">
+
+    <title>Admin Panel</title>
+</head>
+<body>
+    <div class="side-menu">
+        <div class="brand-name">
+            <h1>Brand</h1>
+        </div>
+        <ul>
+            <li><img src="../../assets/images/dashboard%20(2).png" alt="">&nbsp; <span>DashBoard</span></li>
+            <li><img src="../../assets/images/reading-book%20(1).png" alt="">&nbsp; <span>Students</span></li>
+            <li><img src="../../assets/images/teacher2.png" alt="">&nbsp; <span>Teachers</span></li>
+            <li><img src="../../assets/images/school.png" alt="">&nbsp; <span>Schools</span></li>
+            <li><img src="../../assets/images/payment.png" alt="">&nbsp; <span>Income</span></li>
+            <li><img src="../../assets/images/help-web-button.png" alt="">&nbsp; <span>Help</span></li>
+            <li><img src="../../assets/images/settings.png" alt="">&nbsp; <span>Settings</span></li>
+        </ul>
+    </div>
+    <div class="container">
+        <div class="header">
+            <div class="nav">
+                <div class="search">
+                    <input type="text" placeholder="search...">
+                    <button type="submit"><i class="fa fa-search hand-cursor"></i></button>
+                </div>
+                <div class="user">
+                    <a href="#" class="btn">Add New</a>
+                    <i class="fa fa-2x fa-bell"></i>
+                    <div class="im-case">
+                        <i class="fa fa-2x fa-user"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="cards">
+                <div class="card">
+                    <div class="box">
+                        <h1>2193</h1>
+                        <h3>students</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="../../assets/images/students.png" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <h1>53</h1>
+                        <h3>teacher</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="../../assets/images/teachers.png" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <h1>5</h1>
+                        <h3>school</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="../../assets/images/schools.png" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <h1>563000</h1>
+                        <h3>income</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="../../assets/images/income.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="main-content">
+                <div class="recent-payment">
+                    <div class="title">
+                        <h2>Recent payment</h2>
+                        <a href="#" class="btn">View all</a>
+                    </div>
+                    <table>
+                        <tr>
+                            <th>Name</th>
+                            <th>School</th>
+                            <th>Amount</th>
+                            <th>Option</th>
+                        </tr>
+                        <tr>
+                            <td>saeed</td>
+                            <td>St. James College</td>
+                            <td>$120</td>
+                            <td><a href="#" class="btn">View</a></td>
+                        </tr>
+                        <tr>
+                            <td>saeed</td>
+                            <td>St. James College</td>
+                            <td>$120</td>
+                            <td><a href="#" class="btn">View</a></td>
+                        </tr>
+                        <tr>
+                            <td>saeed</td>
+                            <td>St. James College</td>
+                            <td>$120</td>
+                            <td><a href="#" class="btn">View</a></td>
+                        </tr>
+                        <tr>
+                            <td>saeed</td>
+                            <td>St. James College</td>
+                            <td>$120</td>
+                            <td><a href="#" class="btn">View</a></td>
+                        </tr>
+                        <tr>
+                            <td>saeed</td>
+                            <td>St. James College</td>
+                            <td>$120</td>
+                            <td><a href="#" class="btn">View</a></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="new-student">
+                    <div class="title">
+                        <h2>New students</h2>
+                        <a href="#" class="btn">View all</a>
+                    </div>
+                    <table>
+                        <tr>
+                            <th>profile</th>
+                            <th>name</th>
+                            <th>option</th>
+                        </tr>
+                        <tr>
+                            <td><img src="../../assets/images/user.png" alt=""></td>
+                            <td>John</td>
+                        </tr>
+                        <tr>
+                            <td><img src="../../assets/images/user.png" alt=""></td>
+                            <td>John</td>
+                        </tr>
+                        <tr>
+                            <td><img src="../../assets/images/user.png" alt=""></td>
+                            <td>John</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
